42c70e5f116b87aef8026a79d17224a35e1d2c2e
memonsalvo <miguelmonsalvo19@gmail.com>
2023-09-27 03:25:04 -0500

    parametros y modelos

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
     return HttpResponse('Index page')
@@ -9,3 +11,11 @@ def hello(request, id):
 
 def about(request):
     return HttpResponse('About')
+
+def projects(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def tasks(request, title):
+    task = Task.objects.get(title=title)
+    return HttpResponse('task: %s' % task.title)
