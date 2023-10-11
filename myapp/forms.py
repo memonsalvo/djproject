2d03cc42058f319a4ba10be922d0062a33c9c5df
memonsalvo <miguelmonsalvo19@gmail.com>
2023-10-11 02:52:07 -0500

    final

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
 class createNewTask(forms.Form):
-    title = forms.CharField(label="Titulo de tarea", max_length=200)
-    description = forms.CharField(label="Descripcion de la tarea",widget=forms.Textarea)
+    title = forms.CharField(label="Titulo de tarea", 
+        max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
+    description = forms.CharField(label="Descripcion de la tarea",
+        widget=forms.Textarea(attrs={'class' : 'input'}))
 
 class createNewProject(forms.Form):
-    name = forms.CharField(label="nombre del proyecto", max_length=200)
+    name = forms.CharField(label="nombre del proyecto", max_length=200, 
+                           widget=forms.TextInput(attrs={'class' : 'input'}))
--- myapp/views.py
+++ myapp/views.py
@@ -49,4 +49,12 @@ def create_project(request):
         })
     else:
         Project.objects.create(name=request.POST["name"])
-        redirect('projects')
+        return redirect('projects')
+
+def project_detail(request, id):
+    project = get_object_or_404(Project, id=id)
+    tasks = Task.objects.filter(project_id=id)
+    return render(request, 'projects/detail.html',{
+        'project': project,
+        'tasks':tasks
+    })
